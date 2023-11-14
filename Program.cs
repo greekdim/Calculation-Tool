af7fcf806ddcc96fc0012e06b9bc4f159bc66289
greekdim <soultisdimitri@gmail.com>
2023-11-14 11:09:10 +0100

    espace

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ namespace Calc_Tool___Rev_A
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+
         }
     }
 }
